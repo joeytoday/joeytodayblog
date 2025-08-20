00c18bba66043e37e3f4cf574809cdd65c6817f6
joeytoday <joeytoday632@outlook.com>
2025-08-20 18:29:26 +0800

    update sidebar toc

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,5 @@
 
 import __ASTRO_IMAGE_IMPORT_ZhwleN from "./thumbnail.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FMarkdown%E8%AF%AD%E6%B3%95%E6%94%AF%E6%8C%81.md";
-export default new Map([["./thumbnail.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FMarkdown%E8%AF%AD%E6%B3%95%E6%94%AF%E6%8C%81.md", __ASTRO_IMAGE_IMPORT_ZhwleN]]);
+import __ASTRO_IMAGE_IMPORT_E14fM from "/src/assets/projects/notion-temp-para.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025%2Fnotion-temp-para-second-brain.md";
+export default new Map([["./thumbnail.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2FMarkdown%E8%AF%AD%E6%B3%95%E6%94%AF%E6%8C%81.md", __ASTRO_IMAGE_IMPORT_ZhwleN], ["/src/assets/projects/notion-temp-para.jpeg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2F2025%2Fnotion-temp-para-second-brain.md", __ASTRO_IMAGE_IMPORT_E14fM]]);
 		
